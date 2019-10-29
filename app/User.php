fb4a44a5e7ad916da92cbc9685b2dfa3dd54db76
--replace-all <--replace-all>
2019-10-29 17:14:20 +0330

    users with uploading feature working

--- app/User.php
+++ app/User.php
@@ -28,4 +28,9 @@ class User extends Authenticatable
 
         return $this->belongsTo('App\Role');
     }
+
+    public function photos(){
+        return $this->belongsTo('App\Photo');
+
+    }
 }
